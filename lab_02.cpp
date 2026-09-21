2fd3a4816fcc62281116377c3e2edc11d9667d8b
Ivan <ivan.diakovych.ri.2026@lpnu.ua>
2026-09-22 00:00:09 +0300

    first branch_02 commit

--- lab_02.cpp
+++ lab_02.cpp
@@ -27,8 +27,8 @@ int main()
 
     cout << endl;
 
-    cout << "z1 = " << z1 << endl;
-    // cout << "z2 = " << z2 << endl;
+    // cout << "z1 = " << z1 << endl;
+    cout << "z2 = " << z2 << endl;
 
     cin.get();
 
